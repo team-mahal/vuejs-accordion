0e1cf277f2480aed3394e4e5a8059368aa46faef
jual ahmed <md.jual.ah@gmail.com>
2020-04-20 11:16:02 +0600

    make dynamic css

--- src/App.vue
+++ src/App.vue
@@ -35,7 +35,17 @@ export default {
 	data() {
 		return {
 			cssclass: {
-				content:'pl-8 pr-8 pb-5 text-grey-darkest border-l-2 border-indigo-500'
+				content:'pl-8 pr-8 pb-5 text-grey-darkest border-l-2 border-indigo-500',
+				header:{
+					default:'flex justify-between items-center p-5 pl-8 pr-8 cursor-pointer select-none',
+					title:'text-grey-darkest font-thin text-xl',
+					icon:'rounded-full border border-grey w-7 h-7 flex items-center justify-center'
+				},
+				activeheader:{
+					default:'flex justify-between items-center p-5 pl-8 pr-8 cursor-pointer select-none border border-b-1',
+					title:'text-indigo font-thin text-xl',
+					icon:'rounded-full border border border-indigo w-7 h-7 flex items-center justify-center bg-indigo-500'
+				}
 			}
 		}
 	},
--- src/components/Accordion.vue
+++ src/components/Accordion.vue
@@ -1,11 +1,11 @@
 <template>
 <div class="shadow border-b" :class="{ 'not-expanded': !this.expanded }">
-	
-	<header class="flex justify-between items-center p-5 pl-8 pr-8 cursor-pointer select-none" @click="toggleCardState" v-if="!expanded">
-		<span class="text-grey-darkest font-thin text-xl">
+
+	<header :class="cssclass.header.default" @click="toggleCardState" v-if="!expanded">
+		<span :class="cssclass.header.title">
             {{ title }}
         </span>
-		<div class="rounded-full border border-grey w-7 h-7 flex items-center justify-center">
+		<div :class="cssclass.header.icon">
             <!-- icon by feathericons.com -->
             <svg aria-hidden="true" class="" data-reactid="266" fill="none" height="24" stroke="#606F7B" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewbox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
                 <polyline points="6 9 12 15 18 9">
@@ -14,11 +14,11 @@
         </div>
 	</header>
 
-	<header class="flex justify-between items-center p-5 pl-8 pr-8 cursor-pointer select-none border border-b-1" @click="toggleCardState" v-if="expanded">
-        <span class="text-indigo font-thin text-xl">
+	<header :class="cssclass.activeheader.default" @click="toggleCardState" v-if="expanded">
+        <span :class="cssclass.activeheader.title">
             {{ title }}
         </span>
-       <div class="rounded-full border border border-indigo w-7 h-7 flex items-center justify-center bg-indigo-500">
+       <div :class="cssclass.activeheader.icon">
             <svg aria-hidden="true" data-reactid="281" fill="none" height="24" stroke="white" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
                 <polyline points="18 15 12 9 6 15">
                 </polyline>
