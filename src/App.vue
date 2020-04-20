67a8814c6c4f541912a6419854aa4957e2208509
jual ahmed <md.jual.ah@gmail.com>
2020-04-20 11:11:11 +0600

    making dynamic css

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
 <div id="app">
-	<Accordion title="Welcome to Your Vue.js App" @expand="expandAll" class="w-1/2 mx-auto">
+	<Accordion title="Welcome to Your Vue.js App" :cssclass='cssclass' @expand="expandAll" class="w-1/2 mx-auto my-3">
 		<p class="pb-2">Stuff 1</p>
 		<p class="pb-2">Stuff 1</p>
 		<p class="pb-2">Stuff 1</p>
@@ -12,7 +12,7 @@
 		<p class="pb-2">Stuff 1</p>
 		<p class="pb-2">Stuff 1</p>
 	</Accordion>
-	<Accordion title="Welcome to Your Vue.js App" @expand="expandAll" class="w-1/2 mx-auto">
+	<Accordion title="Welcome to Your Vue.js App" :cssclass='cssclass' @expand="expandAll" class="w-1/2 mx-auto my-3">
 		<p class="pb-2">Stuff 1</p>
 		<p class="pb-2">Stuff 1</p>
 		<p class="pb-2">Stuff 1</p>
@@ -32,6 +32,13 @@ import Accordion from './components/Accordion.vue'
 import { EventBus } from './main'
 export default {
 	name: 'App',
+	data() {
+		return {
+			cssclass: {
+				content:'pl-8 pr-8 pb-5 text-grey-darkest border-l-2 border-indigo-500'
+			}
+		}
+	},
 	components: {
 		Accordion
 	},
--- src/components/Accordion.vue
+++ src/components/Accordion.vue
@@ -1,46 +1,35 @@
 <template>
-<div class="shadow">
-	<div class="card border-b" :class="{ 'not-expanded': !this.expanded }">
+<div class="shadow border-b" :class="{ 'not-expanded': !this.expanded }">
+	
+	<header class="flex justify-between items-center p-5 pl-8 pr-8 cursor-pointer select-none" @click="toggleCardState" v-if="!expanded">
+		<span class="text-grey-darkest font-thin text-xl">
+            {{ title }}
+        </span>
+		<div class="rounded-full border border-grey w-7 h-7 flex items-center justify-center">
+            <!-- icon by feathericons.com -->
+            <svg aria-hidden="true" class="" data-reactid="266" fill="none" height="24" stroke="#606F7B" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewbox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+                <polyline points="6 9 12 15 18 9">
+                </polyline>
+            </svg>
+        </div>
+	</header>
 
-		<header class="flex justify-between items-center p-5 pl-8 pr-8 cursor-pointer select-none" @click="toggleCardState" v-if="!expanded">
-			<span class="text-grey-darkest font-thin text-xl">
-                {{ title }}
-            </span>
-			<div class="rounded-full border border-grey w-7 h-7 flex items-center justify-center">
-                <!-- icon by feathericons.com -->
-                <svg aria-hidden="true" class="" data-reactid="266" fill="none" height="24" stroke="#606F7B" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewbox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
-                    <polyline points="6 9 12 15 18 9">
-                    </polyline>
-                </svg>
-            </div>
-		</header>
+	<header class="flex justify-between items-center p-5 pl-8 pr-8 cursor-pointer select-none border border-b-1" @click="toggleCardState" v-if="expanded">
+        <span class="text-indigo font-thin text-xl">
+            {{ title }}
+        </span>
+       <div class="rounded-full border border border-indigo w-7 h-7 flex items-center justify-center bg-indigo-500">
+            <svg aria-hidden="true" data-reactid="281" fill="none" height="24" stroke="white" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+                <polyline points="18 15 12 9 6 15">
+                </polyline>
+            </svg>
+    	</div>
+    </header>
 
-		<header class="flex justify-between items-center p-5 pl-8 pr-8 cursor-pointer select-none" @click="toggleCardState" v-if="expanded">
-            <span class="text-indigo font-thin text-xl">
-                {{ title }}
-            </span>
-           <div class="rounded-full border border border-indigo w-7 h-7 flex items-center justify-center bg-indigo-500">
-	            <!-- icon by feathericons.com -->
-	            <svg aria-hidden="true" data-reactid="281" fill="none" height="24" stroke="white" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
-	                <polyline points="18 15 12 9 6 15">
-	                </polyline>
-	            </svg>
-        	</div>
-        </header>
-
-		<div class="card-content border-b">
-			<div class="content border-l-2 border-indigo-500">
-
-				<div class="border border border-indigo w-7 h-7 flex items-center justify-center bg-indigo">
-					<div>
-                        <div class="pl-8 pr-8 pb-5 text-grey-darkest">
-                           <slot></slot>
-                        </div>
-                    </div>
-				</div>
-			</div>
-		</div>
-
+	<div class="content">
+        <div :class="cssclass.content">
+           <slot></slot>
+        </div>
 	</div>
 </div>
 </template>
@@ -49,7 +38,7 @@
 import { EventBus } from './../main'
 export default {
 	name: 'Accordion',
-	props: ["title", "expandAll"],
+	props: ["title", "expandAll",'cssclass'],
 	data() {
 		return {
 			"expanded": false
@@ -71,21 +60,14 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
- .card .card-header .icon {
-	transform: rotate(180deg);
-	transition: transform 150ms ease-out;
+.content {
+	transition: all 150ms ease;
 }
- .card .card-content {
-	 transition: all 150ms ease;
-}
- .card.not-expanded .card-header .icon {
-	 transform: rotate(0deg);
-}
- .card.not-expanded .card-content {
-	 transform: scaleY(0);
-	 transform-origin: top;
-	 opacity: 0;
-	 height: 0;
-	 padding: 0;
+.not-expanded .content {
+	transform: scaleY(0);
+	transform-origin: top;
+	opacity: 0;
+	height: 0;
+	padding: 0;
 }
 </style>
